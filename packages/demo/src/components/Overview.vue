9b6a77736f7b9cca5db62db5a2dbfc2b1a8964b9
Andrew Whitehead <andrew@1crm.com>
2021-02-21 11:23:21 -0800

    add license info
    Signed-off-by: Andrew Whitehead <andrew@1crm.com>

--- packages/demo/src/components/Overview.vue
+++ packages/demo/src/components/Overview.vue
@@ -2,30 +2,43 @@
   <div class="container">
     <h1>Oceanfront User Interface Library</h1>
     <div class="row">
-      <h2>Lightweight and Flexible</h2>
-      <ul class="col2">
-        <li>
-          The library does not include general page layout rules or
-          element-based selectors, allowing it to be mixed in with other
-          frameworks and keeping the size to a minimum. Visual styles are easily
-          overridden, and multiple basic styles can be used to create themes
-          consistent with various platforms.
-        </li>
-        <li>
-          Components are reused across the framework, allowing for input fields
-          to adapt to a variety of containers and configuration settings. Custom
-          field types only need to be defined once.
-        </li>
-      </ul>
+      <div class="column">
+        <h2>Lightweight and Flexible</h2>
+        <ul class="col2">
+          <li>
+            The library does not include general page layout rules or
+            element-based selectors, allowing it to be mixed in with other
+            frameworks and keeping the size to a minimum. Visual styles are
+            easily overridden, and multiple basic styles can be used to create
+            themes consistent with various platforms.
+          </li>
+          <li>
+            Components are reused across the framework, allowing for input
+            fields to adapt to a variety of containers and configuration
+            settings. Custom field types only need to be defined once.
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="row">
-      <h2>Future Facing</h2>
-      <ul>
-        <li>
-          Built on Typescript and Vue 3, and taking advantage of newer CSS
-          features offered by the current generation of web browsers.
-        </li>
-      </ul>
+      <div class="column">
+        <h2>Future Facing</h2>
+        <ul>
+          <li>
+            Oceanfront is built on Typescript and Vue 3, and takes advantage of
+            newer CSS features offered by the current generation of web
+            browsers.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column">
+        <h2>Open Source</h2>
+        <ul>
+          <li>The library is released under the MIT License.</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
