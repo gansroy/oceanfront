330099b2416f2da06856902e5e1a697a47da0181
Andrew Whitehead <andrew@1crm.com>
2021-02-21 11:13:51 -0800

    separate nav group logic
    Signed-off-by: Andrew Whitehead <andrew@1crm.com>

--- packages/oceanfront/src/components/NavGroup.ts
+++ packages/oceanfront/src/components/NavGroup.ts
@@ -1,99 +1,14 @@
-import { extendConfig } from 'src/lib/config'
-import { defineComponent, SetupContext, computed, reactive, watch } from 'vue'
-import { NavGroupTarget, NavGroupUnregister, setNavGroup } from '../lib/nav'
+import { defineComponent, SetupContext } from 'vue'
+import { extendConfig } from '../lib/config'
+import { reactiveNavGroup, setNavGroup } from '../lib/nav'
 
 export const OfNavGroup = defineComponent({
   name: 'OfNavGroup',
   setup(_props, ctx: SetupContext) {
-    const items = reactive(new Set<NavGroupTarget>())
-    const orderedItems = computed(() =>
-      [...items].sort((a, b) => {
-        const ia = a.elt
-        const ib = b.elt
-        if (!ia) {
-          return ib ? 1 : 0
-        }
-        if (!ib) return -1
-        const cmp = ia.compareDocumentPosition(ib)
-        if (cmp == 4 || cmp == 16) return -1
-        if (cmp == 2 || cmp == 8) return 1
-        return 0
-      })
-    )
-    const scan = computed(() => {
-      let first
-      let lastActive
-      let lastFocused
-      for (const [idx, item] of orderedItems.value.entries()) {
-        if (item.disabled) continue
-        if (first === undefined) first = idx
-        if (item.focused) lastFocused = idx
-        if (item.groupCurrent) lastActive = idx
-      }
-      const active = lastFocused ?? lastActive ?? first
-      return { lastFocused, lastActive, first, active }
-    })
-    const focused = computed(() => {
-      const idx = scan.value.active
-      return idx !== undefined && orderedItems.value[idx]?.focused
-    })
-    watch(
-      () => scan.value,
-      (scan) => {
-        for (const [idx, item] of orderedItems.value.entries()) {
-          const active = idx === scan.active
-          if (item.groupCurrent !== active) item.groupCurrent = active
-        }
-      }
-    )
-
+    const items = reactiveNavGroup()
     extendConfig(() => {
-      setNavGroup({
-        groupRegister(target: NavGroupTarget): NavGroupUnregister | null {
-          items.add(target)
-          return {
-            unregister: () => items.delete(target),
-          }
-        },
-        groupNavigate(
-          target: string | { event: KeyboardEvent } | { id: string }
-        ): boolean {
-          const ordered = orderedItems.value
-          const scanVal = scan.value
-          if (!ordered.length || scanVal.active === undefined) return false
-          const event: KeyboardEvent | undefined = (
-            typeof target === 'object' && (target as any)
-          ).event
-          const key = event?.key
-          if (key === 'ArrowDown' || key === 'ArrowRight') {
-            target = 'next'
-          } else if (key === 'ArrowUp' || key === 'ArrowLeft') {
-            target = 'previous'
-          } else if (event) return false
-          if (target === 'previous' || target === 'next') {
-            let idx = scanVal.active
-            const offs = target === 'next' ? 1 : -1
-            while (true) {
-              idx += offs
-              if (idx < 0) idx = ordered.length - 1
-              else if (idx >= ordered.length) idx = 0
-              if (idx === scanVal.active) return false // looped around
-              if (
-                ordered[idx] &&
-                !ordered[idx].disabled &&
-                ordered[idx].focus
-              ) {
-                ordered[idx].focus!()
-                if (event) event.preventDefault()
-                return true
-              }
-            }
-          }
-          return false
-        },
-      })
+      setNavGroup(items)
     })
-
-    return () => ctx.slots.default?.({ focused })
+    return () => ctx.slots.default?.({ focused: items.groupFocused })
   },
 })
--- packages/oceanfront/src/lib/nav.ts
+++ packages/oceanfront/src/lib/nav.ts
@@ -1,3 +1,4 @@
+import { computed, ComputedRef, reactive, watch } from 'vue'
 import { Config, ConfigManager } from './config'
 import { readonlyUnref } from './util'
 
@@ -15,6 +16,7 @@ export type NavGroupUnregister = {
 }
 
 export type NavGroup = {
+  readonly groupFocused: ComputedRef<boolean>
   groupRegister(target: NavGroupTarget): NavGroupUnregister | null
   groupNavigate(
     target: string | { event: KeyboardEvent } | { id: string }
@@ -54,6 +56,10 @@ export class NavManager implements NavState {
     return !!this.router
   }
 
+  get groupFocused(): ComputedRef<boolean> {
+    return computed(() => (this.group ? this.group.groupFocused.value : false))
+  }
+
   groupRegister(target: NavGroupTarget): NavGroupUnregister | null {
     return (this.group && this.group.groupRegister(target)) || null
   }
@@ -90,4 +96,90 @@ export function setRouter(router: NavRouter | null): void {
 export function useNav(config?: Config): NavState {
   const mgr = configManager.inject(config)
   return readonlyUnref(mgr)
+}
+
+export function reactiveNavGroup(): NavGroup {
+  const items = reactive(new Set<NavGroupTarget>())
+  const orderedItems = computed(() =>
+    [...items].sort((a, b) => {
+      const ia = a.elt
+      const ib = b.elt
+      if (!ia) {
+        return ib ? 1 : 0
+      }
+      if (!ib) return -1
+      const cmp = ia.compareDocumentPosition(ib)
+      if (cmp == 4 || cmp == 16) return -1
+      if (cmp == 2 || cmp == 8) return 1
+      return 0
+    })
+  )
+  const scan = computed(() => {
+    let first
+    let lastActive
+    let lastFocused
+    for (const [idx, item] of orderedItems.value.entries()) {
+      if (item.disabled) continue
+      if (first === undefined) first = idx
+      if (item.focused) lastFocused = idx
+      if (item.groupCurrent) lastActive = idx
+    }
+    const active = lastFocused ?? lastActive ?? first
+    return { lastFocused, lastActive, first, active }
+  })
+  const focused = computed(() => {
+    const idx = scan.value.active
+    return (idx !== undefined && orderedItems.value[idx]?.focused) || false
+  })
+  watch(
+    () => scan.value,
+    (scan) => {
+      for (const [idx, item] of orderedItems.value.entries()) {
+        const active = idx === scan.active
+        if (item.groupCurrent !== active) item.groupCurrent = active
+      }
+    }
+  )
+
+  return {
+    groupFocused: focused,
+    groupRegister(target: NavGroupTarget): NavGroupUnregister | null {
+      items.add(target)
+      return {
+        unregister: () => items.delete(target),
+      }
+    },
+    groupNavigate(
+      target: string | { event: KeyboardEvent } | { id: string }
+    ): boolean {
+      const ordered = orderedItems.value
+      const scanVal = scan.value
+      if (!ordered.length || scanVal.active === undefined) return false
+      const event: KeyboardEvent | undefined = (
+        typeof target === 'object' && (target as any)
+      ).event
+      const key = event?.key
+      if (key === 'ArrowDown' || key === 'ArrowRight') {
+        target = 'next'
+      } else if (key === 'ArrowUp' || key === 'ArrowLeft') {
+        target = 'previous'
+      } else if (event) return false
+      if (target === 'previous' || target === 'next') {
+        let idx = scanVal.active
+        const offs = target === 'next' ? 1 : -1
+        while (true) {
+          idx += offs
+          if (idx < 0) idx = ordered.length - 1
+          else if (idx >= ordered.length) idx = 0
+          if (idx === scanVal.active) return false // looped around
+          if (ordered[idx] && !ordered[idx].disabled && ordered[idx].focus) {
+            ordered[idx].focus!()
+            if (event) event.preventDefault()
+            return true
+          }
+        }
+      }
+      return false
+    },
+  }
 }
