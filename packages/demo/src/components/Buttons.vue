ed71f075ac71c146dabaecde8472c1453e66491d
Alexander Ivanenko <ivanenkoa@gmail.com>
2021-05-24 14:55:58 +0300

    Added abbility to pass a function as a value of menu item; Alow empty button label;

--- packages/demo/src/components/Buttons.vue
+++ packages/demo/src/components/Buttons.vue
@@ -3,37 +3,55 @@
     <h1>Buttons</h1>
     <of-highlight lang="html" :value="sampleCode" />
     <div class="demo-fields">
-    <div class="row">
+      <div class="row">
         <div class="column">
           <h4>Solid</h4>
-          <of-button />
+          <of-button>Submit</of-button>
           <of-button disabled>Disabled</of-button>
           <of-button icon="gear">With Icon</of-button>
           <of-button rounded>Rounded</of-button>
-          <of-button split :items="testItems" />
-          <of-button icon="gear" rounded split :items="testItems" />
+          <of-button split :items="testItems">Menu</of-button>
+          <of-button
+            icon="gear"
+            rounded
+            split
+            :items="testItems"
+            :on-click-menu-item="selectItem"
+            >Menu</of-button
+          >
         </div>
       </div>
       <div class="row">
         <div class="column">
           <h4>Outlined</h4>
-          <of-button variant="outlined" />
+          <of-button variant="outlined">Submit</of-button>
           <of-button variant="outlined" disabled>Disabled</of-button>
           <of-button variant="outlined" icon="gear">With Icon</of-button>
           <of-button variant="outlined" rounded>Rounded</of-button>
-          <of-button variant="outlined" split :items="testItems" />
-          <of-button variant="outlined" icon="gear" rounded split :items="testItems" />
+          <of-button variant="outlined" split :items="testItems"
+            >Menu</of-button
+          >
+          <of-button
+            variant="outlined"
+            icon="gear"
+            rounded
+            split
+            :items="testItems"
+            >Menu</of-button
+          >
         </div>
       </div>
       <div class="row">
         <div class="column">
           <h4>Text</h4>
-          <of-button variant="text" />
+          <of-button variant="text">Submit</of-button>
           <of-button variant="text" disabled>Disabled</of-button>
           <of-button variant="text" icon="gear">With Icon</of-button>
           <of-button variant="text" rounded>Rounded</of-button>
-          <of-button variant="text" split :items="testItems" />
-          <of-button variant="text" icon="gear" rounded split :items="testItems" />
+          <of-button variant="text" split :items="testItems">Menu</of-button>
+          <of-button variant="text" icon="gear" rounded split :items="testItems"
+            >Menu</of-button
+          >
         </div>
       </div>
       <div class="row">
@@ -42,7 +60,9 @@
           <span class="of-buttonset">
             <of-button icon="accept">Save</of-button>
             <of-button icon="refresh" disabled>Refresh</of-button>
-            <of-button icon="gear" rounded split :items="testItems" />
+            <of-button icon="gear" rounded split :items="testItems"
+              >Menu</of-button
+            >
             <of-button icon="cancel">Cancel</of-button>
           </span>
         </div>
@@ -58,40 +78,49 @@ export default defineComponent({
   setup() {
     const sampleCode = `
 <!-- Solid -->
-<of-button />
+<of-button>Submit</of-button>
 <of-button disabled>Disabled</of-button>
 <of-button icon="gear">With Icon</of-button>
 <of-button rounded>Rounded</of-button>
-<of-button split :items="testItems" />
-<of-button icon="gear" rounded split :items="testItems" />
+<of-button split :items="testItems">Menu</of-button>
+<of-button icon="gear" rounded split :items="testItems">Menu</of-button>
 <!-- Outlined -->
-<of-button variant="outlined" />
+<of-button variant="outlined">Submit</of-button>
 <of-button variant="outlined" disabled>Disabled</of-button>
 <of-button variant="outlined" icon="gear">With Icon</of-button>
 <of-button variant="outlined" rounded>Rounded</of-button>
-<of-button variant="outlined" split :items="testItems" />
-<of-button variant="outlined" icon="gear" rounded split :items="testItems" />
+<of-button variant="outlined" split :items="testItems">Menu</of-button>
+<of-button variant="outlined" icon="gear" rounded split :items="testItems">Menu</of-button>
 <!-- Text -->
-<of-button variant="text" />
+<of-button variant="text">Submit</of-button>
 <of-button variant="text" disabled>Disabled</of-button>
 <of-button variant="text" icon="gear">With Icon</of-button>
 <of-button variant="text" rounded>Rounded</of-button>
-<of-button variant="text" split :items="testItems" />
-<of-button variant="text" icon="gear" rounded split :items="testItems" />
+<of-button variant="text" split :items="testItems">Menu</of-button>
+<of-button variant="text" icon="gear" rounded split :items="testItems">Menu</of-button>
 <!-- Set -->
 <span class="of-buttonset">
   <of-button icon="accept">Save</of-button>
   <of-button icon="refresh" disabled>Refresh</of-button>
-  <of-button icon="gear" rounded split :items="testItems" />
+  <of-button icon="gear" rounded split :items="testItems">Menu</of-button>
   <of-button icon="cancel">Cancel</of-button>
 </span>`
+
+    const selectMenu = () => {
+      console.log('Menu item selected')
+    }
+
+    const selectItem = () => {
+      console.log('Coomon onClick menu item handler')
+    }
+
     const testItems = [
       { special: 'header', text: 'Test select items1' },
-      { text: 'Option 1', value: 'o1' },
+      { text: 'Option 1', value: selectMenu },
       { special: 'divider' },
       { text: 'Option 2', value: 'o2' },
     ]
-    return { sampleCode, testItems }
+    return { sampleCode, testItems, selectItem }
   },
 })
 </script>
