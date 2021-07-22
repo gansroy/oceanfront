3c9c3cffc88245d57f9b338c0bdc39ee327842fc
Andrey Demenev <andrey@1crm.com>
2021-07-22 22:45:06 +1000

    small change to test auto updates

--- packages/oceanfront/src/lib/calendar/index.ts
+++ packages/oceanfront/src/lib/calendar/index.ts
@@ -41,6 +41,7 @@ export function withZeroTime(ts: Timestamp): Timestamp {
 }
 
 export function compareTimestamps(a: Timestamp, b: Timestamp): number {
+    console.log(1);
     let delta = a.year - b.year
     if (delta != 0) return Math.sign(delta)
     delta = a.month - b.month
