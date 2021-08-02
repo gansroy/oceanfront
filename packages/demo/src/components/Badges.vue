f95500ba2d99111a25eca54395ba044967beccdd
Andrey Demenev <andrey@1crm.com>
2021-08-03 01:45:47 +1000

    circular and icon badges

--- packages/demo/src/components/Badges.vue
+++ packages/demo/src/components/Badges.vue
@@ -5,6 +5,15 @@
     <of-highlight lang="html" :value="sampleCode" />
     <h2>States</h2>
     <of-badge v-for="status in statusList" :status="status" :key="status">{{status}}</of-badge>
+    <h2>Icons</h2>
+    <of-badge v-for="status in statusList" :status="status" :key="status"><of-icon name="print" />{{status}}</of-badge>
+    <h2>Circular</h2>
+    <of-badge v-for="status in statusList" circular :status="status" :key="status">{{status}}</of-badge>
+    <br>
+    <of-badge v-for="status in statusList" circular :status="status" :key="status"><of-icon name="print" />{{status}}</of-badge>
+    <h2>Icon only</h2>
+    <of-badge icon :status="info"><of-icon name="print" /></of-badge>
+    <of-badge circular icon :status="info"><of-icon name="print" /></of-badge>
   </div>
 </template>
 
@@ -21,8 +30,11 @@ import { defineComponent } from 'vue'
     const sampleCode = `
 <!-- State -->
 <of-badge status="info">Info</of-badge>
+<of-badge status="info"><of-icon name="print" />Info</of-badge>
+<of-badge circular status="info">Info</of-badge>
+<of-badge icon status="info"><of-icon name="print" /></of-badge>
+<of-badge icon circular status="info"><of-icon name="print" /></of-badge>
 `
-
 export default defineComponent({
     setup() {
         return {
--- packages/oceanfront/src/components/Badge.vue
+++ packages/oceanfront/src/components/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="badge" :class="stateClass"><slot></slot></div>
+    <div class="badge" :class="{[stateClass] : true, circular, icon}"><slot></slot></div>
 </template>
 
 <script lang="ts">
@@ -15,30 +15,18 @@ type Status =
 export default defineComponent({
     props: {
         status: String as PropType<Status>,
+        circular: Boolean,
+        icon: Boolean,
     },
     setup(props) {
         const stateClass = computed( () => "state-" + props.status);
+        const circular = computed( () => props.circular);
+        const icon = computed( () => props.icon);
         return {
-            stateClass
+            stateClass,
+            circular,
+            icon,
         }
     },
 })
 </script>
-
-
-<style>
-.badge {
-    display: inline-block; 
-    padding-left: 1ex;
-    padding-right: 1ex;
-    border: solid 1px;
-    border-color: var(--border-color-badge); 
-    color: var(--color-badge);
-    background: var(--bg-badge);
-    border-radius: 14px;
-    height: 28px;
-    margin-left: 0.25ex;
-    margin-right: 0.25ex;
-}
-
-</style>
