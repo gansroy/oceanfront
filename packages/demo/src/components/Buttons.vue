39d2c14dd47f610959a47fec38f813385f2a9803
Ivan Bibik <ivan.bibik@gmail.com>
2021-08-27 10:02:20 +0500

    added new button type

--- packages/demo/src/components/Buttons.vue
+++ packages/demo/src/components/Buttons.vue
@@ -5,53 +5,12 @@
     <div class="demo-fields">
       <div class="row">
         <div class="column">
-          <h4>Solid</h4>
           <of-button>Submit</of-button>
           <of-button disabled>Disabled</of-button>
           <of-button icon="gear">With Icon</of-button>
           <of-button rounded>Rounded</of-button>
-          <of-button split :items="testItems">Menu</of-button>
-          <of-button
-            icon="gear"
-            rounded
-            split
-            :items="testItems"
-            :on-click-menu-item="selectItem"
-            >Menu</of-button
-          >
-        </div>
-      </div>
-      <div class="row">
-        <div class="column">
-          <h4>Outlined</h4>
-          <of-button variant="outlined">Submit</of-button>
-          <of-button variant="outlined" disabled>Disabled</of-button>
-          <of-button variant="outlined" icon="gear">With Icon</of-button>
-          <of-button variant="outlined" rounded>Rounded</of-button>
-          <of-button variant="outlined" split :items="testItems"
-            >Menu</of-button
-          >
-          <of-button
-            variant="outlined"
-            icon="gear"
-            rounded
-            split
-            :items="testItems"
-            >Menu</of-button
-          >
-        </div>
-      </div>
-      <div class="row">
-        <div class="column">
-          <h4>Text</h4>
-          <of-button variant="text">Submit</of-button>
-          <of-button variant="text" disabled>Disabled</of-button>
-          <of-button variant="text" icon="gear">With Icon</of-button>
-          <of-button variant="text" rounded>Rounded</of-button>
-          <of-button variant="text" split :items="testItems">Menu</of-button>
-          <of-button variant="text" icon="gear" rounded split :items="testItems"
-            >Menu</of-button
-          >
+          <of-button split :items="testItems" :on-click-menu="menuClick">Split</of-button>
+          <of-button icon="gear" rounded :items="testItems">Menu</of-button>
         </div>
       </div>
       <div class="row">
@@ -60,11 +19,23 @@
           <span class="of-buttonset">
             <of-button icon="accept">Save</of-button>
             <of-button icon="refresh" disabled>Refresh</of-button>
-            <of-button icon="gear" rounded split :items="testItems"
-              >Menu</of-button
-            >
+            <of-button icon="gear" rounded split :items="testItems">Split</of-button>
             <of-button icon="cancel">Cancel</of-button>
           </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column sm-1">
+          <h4>Solid</h4>
+          <of-button>Submit</of-button>
+        </div>
+        <div class="column sm-1">
+          <h4>Outlined</h4>
+          <of-button variant="outlined">Submit</of-button>
+        </div>
+        <div class="column sm-1">
+          <h4>Text</h4>
+          <of-button variant="text">Submit</of-button>
         </div>
       </div>
     </div>
@@ -77,50 +48,46 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
     const sampleCode = `
-<!-- Solid -->
 <of-button>Submit</of-button>
 <of-button disabled>Disabled</of-button>
 <of-button icon="gear">With Icon</of-button>
 <of-button rounded>Rounded</of-button>
-<of-button split :items="testItems">Menu</of-button>
-<of-button icon="gear" rounded split :items="testItems">Menu</of-button>
-<!-- Outlined -->
-<of-button variant="outlined">Submit</of-button>
-<of-button variant="outlined" disabled>Disabled</of-button>
-<of-button variant="outlined" icon="gear">With Icon</of-button>
-<of-button variant="outlined" rounded>Rounded</of-button>
-<of-button variant="outlined" split :items="testItems">Menu</of-button>
-<of-button variant="outlined" icon="gear" rounded split :items="testItems">Menu</of-button>
-<!-- Text -->
-<of-button variant="text">Submit</of-button>
-<of-button variant="text" disabled>Disabled</of-button>
-<of-button variant="text" icon="gear">With Icon</of-button>
-<of-button variant="text" rounded>Rounded</of-button>
-<of-button variant="text" split :items="testItems">Menu</of-button>
-<of-button variant="text" icon="gear" rounded split :items="testItems">Menu</of-button>
+<of-button split :items="testItems" :on-click-menu="menuClick">Split</of-button>
+<of-button icon="gear" rounded :items="testItems">Menu</of-button>
 <!-- Set -->
 <span class="of-buttonset">
   <of-button icon="accept">Save</of-button>
   <of-button icon="refresh" disabled>Refresh</of-button>
-  <of-button icon="gear" rounded split :items="testItems">Menu</of-button>
+  <of-button icon="gear" rounded split :items="testItems">Split</of-button>
   <of-button icon="cancel">Cancel</of-button>
-</span>`
+</span>
+<!-- Solid -->
+<of-button>Submit</of-button>
+<!-- Outlined -->
+<of-button variant="outlined">Submit</of-button>
+<!-- Text -->
+<of-button variant="text">Submit</of-button>`
 
-    const selectMenu = () => {
-      console.log('Menu item selected')
+    const selectMenu1 = () => {
+      console.log('Menu item 1 selected')
     }
-
-    const selectItem = () => {
-      console.log('Coomon onClick menu item handler')
+    const selectMenu2 = () => {
+      console.log('Menu item 2 selected')
     }
-
+    const menuClick = () => {
+      console.log('Menu click')
+    }
     const testItems = [
-      { special: 'header', text: 'Test select items1' },
-      { text: 'Option 1', value: selectMenu, attrs: { 'data-test': 'my-btn' } },
-      { special: 'divider' },
-      { text: 'Option 2', value: 'o2' },
+      //{ special: 'header', text: 'Test select items1' },
+      { text: 'Option 1', value: selectMenu1, attrs: { 'data-test': 'my-btn' } },
+      //{ special: 'divider' },
+      { text: 'Option 2', value: selectMenu2 },
     ]
-    return { sampleCode, testItems, selectItem }
+    return { 
+      sampleCode, 
+      testItems, 
+      menuClick
+    }
   },
 })
 </script>
