7dee0d33233908f83f5835a892e025eeaf74b839
Andrew Whitehead <cywolf@gmail.com>
2021-12-11 14:44:42 -0800

    use jest-matchmedia-mock
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- packages/oceanfront/jest.setup.js
+++ packages/oceanfront/jest.setup.js
@@ -1,14 +1,3 @@
-// temporary manual mock (missing from JSDOM)
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+import MatchMediaMock from 'jest-matchmedia-mock';
+
+new MatchMediaMock()
