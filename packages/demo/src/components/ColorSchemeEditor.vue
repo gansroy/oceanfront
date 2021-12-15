04ddee763dc27ede324452c79684ed60c3cfed47
Andrey Demenev <andrey@1crm.com>
2021-12-15 23:18:17 +1000

    display application pallete in colors editor

--- packages/demo/src/components/ColorSchemeEditor.vue
+++ packages/demo/src/components/ColorSchemeEditor.vue
@@ -1,33 +1,54 @@
 <template>
   <div class="container">
     <h1>Color scheme editor</h1>
-    <div v-for="(color, name) in colors" :key="name" class="row content">
-      <div class="column" style="max-width: 10em">
-        <of-field
-          v-model="colors[name]"
-          type="color"
-          :label="formatName(name)"
-        />
+    <div class="row content">
+      <div class="column spaced" style="max-width: 15em">
+        <div style="padding: 0 1rem">
+          <h2>Source colors</h2>
+          <div v-for="(color, name) in colors" :key="name" class="row content">
+            <div class="column" style="max-width: 10em">
+              <of-field
+                v-model="colors[name]"
+                type="color"
+                :label="formatName(name)"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div style="width: 2em"></div>
-      <div class="column">
-        &nbsp;
-        <div style="border: solid 1px #fff; display: flex; width: fit-content">
-          <div
-            style="border: solid 1px #000; display: flex; width: fit-content"
-          >
-            <div
-              v-for="(cl, idx) in pallettes[name]"
-              :key="idx"
-              :style="{
-                height: '30px',
-                width: '3px',
-                background: cl.color,
-                'align-items': 'center',
-                'justify-content': 'center',
-                display: 'flex',
-              }"
-            />
+      <div class="column spaced">
+        <div style="padding: 0 1rem">
+          <h2>Application color palette</h2>
+          <div class="palette-grid of--elevated-2" :style="styles">
+            <div class="primary">Primary</div>
+            <div class="on-primary">On Primary</div>
+            <div class="primary-container">Primary Container</div>
+            <div class="on-primary-container">On Primary Container</div>
+            <div class="secondary">Secondary</div>
+            <div class="on-secondary">On Secondary</div>
+            <div class="secondary-container">Secondary Container</div>
+            <div class="on-secondary-container">On Secondary Container</div>
+            <div class="tertiary">Tertiary</div>
+            <div class="on-tertiary">On Tertiary</div>
+            <div class="tertiary-container">Tertiary Container</div>
+            <div class="on-tertiary-container">On Tertiary Container</div>
+            <div class="error">Error</div>
+            <div class="on-error">On Error</div>
+            <div class="error-container">Error Container</div>
+            <div class="on-error-container">On Error Container</div>
+            <div class="background">Background</div>
+            <div class="on-background">On Background</div>
+            <div class="surface">Surface</div>
+            <div class="on-surface">On Surface</div>
+            <div class="surface-variant">Surface Variant</div>
+            <div class="on-surface-variant">On Surface Variant</div>
+            <div class="inverse-surface">Inverse Surface</div>
+            <div class="inverse-on-surface">Inverse On Surface</div>
+            <div class="outline">Outline</div>
+            <div class="shadow">Shadow</div>
+            <div class="inverse-primary">Inverse Primary</div>
+            <div class="inverse-secondary">Inverse Secondary</div>
+            <div class="inverse-tertiary">Inverse Tertiary</div>
           </div>
         </div>
       </div>
@@ -170,9 +191,9 @@ const ColorSchemeEditor = defineComponent({
       style += `--of-color-inverse-secondary-dark: ${pallettes.value['secondary'][10].color};\n`
       style += `--of-color-inverse-tertiary-dark: ${pallettes.value['tertiary'][10].color};\n`
 
-      const htmlEl = document.body.parentElement
-      if (htmlEl) {
-        htmlEl.setAttribute('style', style)
+      const styleEl = document.querySelector('style#color-scheme')
+      if (styleEl) {
+        ;(styleEl as HTMLElement).innerText = `:root {${style}}`
       }
 
       return style
@@ -199,7 +220,150 @@ const ColorSchemeEditor = defineComponent({
       styles,
     }
   },
+  mounted() {
+    if (!document.querySelector('style#color-scheme')) {
+      const style = document.createElement('style')
+      style.id = 'color-scheme'
+      document.head.append(style)
+    }
+  },
 })
 
 export default ColorSchemeEditor
 </script>
+
+<style lang="scss" scoped>
+.palette-grid {
+  display: grid;
+  font-size: 65%;
+  grid-template-columns: repeat(4, minmax(50px, 1fr));
+  border-radius: 4px;
+  overflow: hidden;
+  & div {
+    padding: 0.4rem;
+    font-weight: bold;
+  }
+  & .primary {
+    background: var(--of-color-primary);
+    color: var(--of-color-on-primary);
+  }
+  & .on-primary {
+    color: var(--of-color-primary);
+    background: var(--of-color-on-primary);
+  }
+  & .primary-container {
+    background: var(--of-color-primary-container);
+    color: var(--of-color-on-primary-container);
+  }
+  & .on-primary-container {
+    color: var(--of-color-primary-container);
+    background: var(--of-color-on-primary-container);
+  }
+  & .secondary {
+    background: var(--of-color-secondary);
+    color: var(--of-color-on-secondary);
+  }
+  & .on-secondary {
+    color: var(--of-color-secondary);
+    background: var(--of-color-on-secondary);
+  }
+  & .secondary-container {
+    background: var(--of-color-secondary-container);
+    color: var(--of-color-on-secondary-container);
+  }
+  & .on-secondary-container {
+    color: var(--of-color-secondary-container);
+    background: var(--of-color-on-secondary-container);
+  }
+  & .tertiary {
+    background: var(--of-color-tertiary);
+    color: var(--of-color-on-tertiary);
+  }
+  & .on-tertiary {
+    color: var(--of-color-tertiary);
+    background: var(--of-color-on-tertiary);
+  }
+  & .tertiary-container {
+    background: var(--of-color-tertiary-container);
+    color: var(--of-color-on-tertiary-container);
+  }
+  & .on-tertiary-container {
+    color: var(--of-color-tertiary-container);
+    background: var(--of-color-on-tertiary-container);
+  }
+  & .error {
+    background: var(--of-color-error);
+    color: var(--of-color-on-error);
+  }
+  & .on-error {
+    color: var(--of-color-error);
+    background: var(--of-color-on-error);
+  }
+  & .error-container {
+    background: var(--of-color-error-container);
+    color: var(--of-color-on-error-container);
+  }
+  & .on-error-container {
+    color: var(--of-color-error-container);
+    background: var(--of-color-on-error-container);
+  }
+  & .background {
+    background: var(--of-color-background);
+    color: var(--of-color-on-background);
+  }
+  & .on-background {
+    color: var(--of-color-background);
+    background: var(--of-color-on-background);
+  }
+  & .surface {
+    background: var(--of-color-surface);
+    color: var(--of-color-on-surface);
+  }
+  & .on-surface {
+    color: var(--of-color-surface);
+    background: var(--of-color-on-surface);
+  }
+  & .outline {
+    grid-column-start: 1;
+    grid-column-end: 3;
+    background: var(--of-color-outline);
+    color: var(--of-color-on-background);
+  }
+  & .surface-variant {
+    background: var(--of-color-surface-variant);
+    color: var(--of-color-on-surface-variant);
+  }
+  & .on-surface-variant {
+    color: var(--of-color-surface-variant);
+    background: var(--of-color-on-surface-variant);
+  }
+  & .shadow {
+    grid-column-start: 3;
+    grid-column-end: 5;
+    color: var(--of-color-background);
+    background: var(--of-color-shadow);
+  }
+  & .inverse-surface {
+    background: var(--of-color-inverse-surface);
+    color: var(--of-color-inverse-on-surface);
+  }
+  & .inverse-on-surface {
+    color: var(--of-color-inverse-surface);
+    background: var(--of-color-inverse-on-surface);
+  }
+  & .inverse-primary {
+    grid-column-start: 1;
+    grid-column-end: 3;
+    color: var(--of-color-primary);
+    background: var(--of-color-inverse-primary);
+  }
+  & .inverse-secondary {
+    color: var(--of-color-secondary);
+    background: var(--of-color-inverse-secondary);
+  }
+  & .inverse-tertiary {
+    color: var(--of-color-tertiary);
+    background: var(--of-color-inverse-tertiary);
+  }
+}
+</style>
