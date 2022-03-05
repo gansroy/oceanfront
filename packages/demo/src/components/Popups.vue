c6841af1f7837d7d11297227b71811c9e90b759a
Andrey Demenev <andrey@1crm.com>
2022-03-05 16:19:42 +1000

    fix nested overlays

--- packages/demo/src/components/Popups.vue
+++ packages/demo/src/components/Popups.vue
@@ -8,7 +8,16 @@
       <div class="container">
         <h4>Test Dialog</h4>
         <of-select-field :items="['one', 'two']" label="Test select" />
+        <p>
+          <of-button @click="toggleNestedDialog">Show nested dialog</of-button>
+        </p>
       </div>
+      <of-dialog v-model="nestedDialogActive">
+        <div class="container">
+          <h4>Nested Dialog</h4>
+          <of-select-field :items="['one', 'two']" label="Test select" />
+        </div>
+      </of-dialog>
     </of-dialog>
   </div>
 </template>
@@ -24,12 +33,17 @@ export default defineComponent({
 />
 `
     const dialogActive = ref(false)
+    const nestedDialogActive = ref(false)
 
     return {
       dialogActive,
+      nestedDialogActive,
       sampleCode,
       toggleDialog: () => {
         dialogActive.value = !dialogActive.value
+      },
+      toggleNestedDialog: () => {
+        nestedDialogActive.value = !nestedDialogActive.value
       },
     }
   },
